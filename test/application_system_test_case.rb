54aa33472be0a4a603c4279282cf5a19977d4920
AlyanTha <81434430+AlyanTha@users.noreply.github.com>
2025-02-01 22:29:26 -0600

    roboCop

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |options|
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ] do |options|
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
